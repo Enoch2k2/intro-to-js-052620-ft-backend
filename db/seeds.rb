730e1584fd12eae71c99d01a2c34218318f4e3b1
Enoch2k2 <enoch2k2@hotmail.com>
2020-08-25 17:03:35 -0500

    added blogs to project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Blog.create(title: "New BLog", content: "This content")
+Blog.create(title: "Some Blog", content: "That content")
+Blog.create(title: "This Blog", content: "Exciting Content")
+Blog.create(title: "That Blog", content: "Interesting Content")
+Blog.create(title: "An old blog", content: "Old Content")
